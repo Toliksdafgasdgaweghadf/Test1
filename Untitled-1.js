8655f41afc1bc8801d1b6dec5a386777c5f6d3a4
Toliksdafgasdgaweghadf <87318895+Toliksdafgasdgaweghadf@users.noreply.github.com>
2021-07-12 17:11:46 +0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,32 @@
+console.log('Hello, world!')
+// dcgdfgdgfdg
+var number =5;
+console.log(number);
+
+var mynaMe = "ttt";
+console.log(mynaMe);
+
+mynaMe = 8;
+console.log(mynaMe);
+
+let ourName = "hhghfgh";
+
+console.log("               ");
+
+var a = 5;
+var b = 10;
+var c = "string";
+
+a = a+1;
+b = b+5;
+c = c+"string!";
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.log("               ");
+
+a++;
+console.log(a);
+
