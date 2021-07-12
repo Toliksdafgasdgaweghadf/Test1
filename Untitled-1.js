003bd51fecc7030df199db52e4ee5fc8ae15c309
Toliksdafgasdgaweghadf <87318895+Toliksdafgasdgaweghadf@users.noreply.github.com>
2021-07-12 22:33:18 +0300

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -30,3 +30,43 @@ console.log("               ");
 a++;
 console.log(a);
 
+a =a + 2.5;
+
+console.log(a);
+
+a = a *0.5;
+console.log(a);
+
+b = a/2;
+console.log(b);
+
+b = 11%3;
+console.log(b);
+
+a+=2;
+console.log(a);
+
+a*=3;
+console.log(a);
+
+a/=4;
+console.log(a);
+
+console.log("dgdfgdfg \"dfgdfgdfg\"");
+
+console.log('dgdfgdfg "dfgdfgdfg"');
+
+console.log(`dgdf'gdf'g \"dfgdfgdfg\"`);
+
+var FirstString = "First String \n Second String"
+console.log(FirstString);
+
+var Fstring = "1a";
+var Sstring = "2b";
+console.log(Fstring + Sstring);
+
+console.log(Fstring[0]);
+
+var Test2 = Fstring[Fstring.length - 1];
+console.log(Test2);
+
---
+++ Untitled-2.js
@@ -0,0 +1,56 @@
+function WordBlanks (myNoun, myAdjective, myVerb, myAdverb){
+
+    var result = "";
+
+    result += "The " + myAdjective +" "+ myNoun +" "+ myVerb + " to the store " + myAdverb;
+
+    return result;
+
+};
+
+console.log(WordBlanks("dog","big", "ran", "quickly"));
+console.log(WordBlanks("bike","slow", "flew", "slowly"));
+
+var ourArray  = ["John", 23];
+var myArray = ["Quincy",1];
+var newArray = [["univ",42],[ "ewr" ,101010]];
+
+console.log(newArray[0][0]);
+
+newArray.push(["hippi","jam"]);
+
+console.log(newArray[2][0]);
+
+var new2Array = newArray.pop(); //Удаляет последний элемент массива или переносит его в другой массив
+console.log(newArray.length);
+console.log(new2Array[0]);
+
+newArray.push(["hippi","jam"]);
+var new3Array = newArray.shift(); //Удаляет первый элемент массива или переносит его в другой массив
+console.log(newArray.length);
+console.log(new3Array[0]);
+console.log("\n");
+
+newArray.unshift(["ЧгnIv",42]);  //Добавляет первый элемент в массив
+console.log(newArray.length);
+console.log(newArray[0][0]);
+console.log("\n");
+
+var myList = [["Cereal",3],["Milk",2],["Bananas",3],["Juice",2],["Eggs",12]];
+
+function ourReusableFunction() {
+    console.log("hfghdh!");
+};
+
+ourReusableFunction(); 
+
+function FunctionWithArgs(a,b) {
+    console.log(a+b);
+    oopsGolbal = 3; //Такое определение автоматически сделает переменную глобальной
+};
+
+function FunctionWithArgs2(a,b) {
+    console.log(a-b);
+};
+
+FunctionWithArgs(2,4);
